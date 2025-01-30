c278588cddd3be6a9c840d6496a04a308eb29be5
Nob2222 <dendendenso.2@gmail.com>
2025-01-31 01:52:45 +0900

    グラフの削除、プロンプトと音声の変更

--- src/components/altair/Altair.tsx
+++ src/components/altair/Altair.tsx
@@ -15,28 +15,28 @@
  */
 import { type FunctionDeclaration, SchemaType } from "@google/generative-ai";
 import { useEffect, useRef, useState, memo } from "react";
-import vegaEmbed from "vega-embed";
+// import vegaEmbed from "vega-embed";
 import { useLiveAPIContext } from "../../contexts/LiveAPIContext";
 import { ToolCall } from "../../multimodal-live-types";
 
-const declaration: FunctionDeclaration = {
-  name: "render_altair",
-  description: "Displays an altair graph in json format.",
-  parameters: {
-    type: SchemaType.OBJECT,
-    properties: {
-      json_graph: {
-        type: SchemaType.STRING,
-        description:
-          "JSON STRING representation of the graph to render. Must be a string, not a json object",
-      },
-    },
-    required: ["json_graph"],
-  },
-};
+// const declaration: FunctionDeclaration = {
+//   name: "render_altair",
+//   description: "Displays an altair graph in json format.",
+//   parameters: {
+//     type: SchemaType.OBJECT,
+//     properties: {
+//       json_graph: {
+//         type: SchemaType.STRING,
+//         description:
+//           "JSON STRING representation of the graph to render. Must be a string, not a json object",
+//       },
+//     },
+//     required: ["json_graph"],
+//   },
+// };
 
 function AltairComponent() {
-  const [jsonString, setJSONString] = useState<string>("");
+  // const [jsonString, setJSONString] = useState<string>("");
   const { client, setConfig } = useLiveAPIContext();
 
   useEffect(() => {
@@ -45,20 +45,21 @@ function AltairComponent() {
       generationConfig: {
         responseModalities: "audio",
         speechConfig: {
-          voiceConfig: { prebuiltVoiceConfig: { voiceName: "Aoede" } },
+          voiceConfig: { prebuiltVoiceConfig: { voiceName: "Puck" } }, //"Puck" | "Charon" | "Kore" | "Fenrir" | "Aoede" 
         },
       },
       systemInstruction: {
         parts: [
           {
-            text: 'You are my helpful assistant. Any time I ask you for a graph call the "render_altair" function I have provided you. Dont ask for additional information just make your best judgement.',
+            // text: 'You are my helpful assistant. Any time I ask you for a graph call the "render_altair" function I have provided you. Dont ask for additional information just make your best judgement.',
+            text: '私は視覚障害者で目が見えません。動画を見せるので、周囲の状況を私にわかりやすく伝えてください。特徴的なものがあれば教えてください。',
           },
         ],
       },
       tools: [
         // there is a free-tier quota for search
         { googleSearch: {} },
-        { functionDeclarations: [declaration] },
+        // { functionDeclarations: [declaration] },
       ],
     });
   }, [setConfig]);
@@ -66,13 +67,13 @@ function AltairComponent() {
   useEffect(() => {
     const onToolCall = (toolCall: ToolCall) => {
       console.log(`got toolcall`, toolCall);
-      const fc = toolCall.functionCalls.find(
-        (fc) => fc.name === declaration.name,
-      );
-      if (fc) {
-        const str = (fc.args as any).json_graph;
-        setJSONString(str);
-      }
+      // const fc = toolCall.functionCalls.find(
+      //   (fc) => fc.name === declaration.name,
+      // );
+      // if (fc) {
+      //   const str = (fc.args as any).json_graph;
+      //   setJSONString(str);
+      // }
       // send data for the response of your tool call
       // in this case Im just saying it was successful
       if (toolCall.functionCalls.length) {
@@ -94,14 +95,15 @@ function AltairComponent() {
     };
   }, [client]);
 
-  const embedRef = useRef<HTMLDivElement>(null);
+  // const embedRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (embedRef.current && jsonString) {
-      vegaEmbed(embedRef.current, JSON.parse(jsonString));
-    }
-  }, [embedRef, jsonString]);
-  return <div className="vega-embed" ref={embedRef} />;
+  // useEffect(() => {
+  //   if (embedRef.current && jsonString) {
+  //     vegaEmbed(embedRef.current, JSON.parse(jsonString));
+  //   }
+  // }, [embedRef, jsonString]);
+  // return <div className="vega-embed" ref={embedRef} />;
+  return null;
 }
 
 export const Altair = memo(AltairComponent);
